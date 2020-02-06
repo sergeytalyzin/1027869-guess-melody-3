fb3adf27c46d87a9277b6081c37a2dec2b40263d
Sergey <st@ttsgroup.su>
2020-02-06 15:52:58 +0300

    mod1-task2

---
+++ src/components/app/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx"
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorsCount} = props;
+  return (
+    <WelcomeScreen
+      errorsCount = {errorsCount}
+    />
+  );
+};
+
+export default App;
